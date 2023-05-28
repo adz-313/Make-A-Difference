7bb8b29136ab1288963e9acce650a4b631b01c3b
Aditya <ads73818@gmail.com>
2023-05-28 10:37:11 +0530

    Testing github branch protection changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,6 +5,3 @@ const API = axios.create({ baseURL:'https://mad-dj-app.herokuapp.com/' });
 export const createDrive = (drive) => API.post('drive/', drive);
 
 export const recordTransaction = (address, donation) => API.post(`drive/${address}/history`, donation);
-
-
-
